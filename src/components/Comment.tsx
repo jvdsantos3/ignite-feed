afea578c3438d212149de8e7e5f1f2701a2593f2
João <jv.dossantos012@gmail.com>
2023-02-01 19:30:06 -0300

    Correção de bugs

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -23,7 +23,7 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
     return (
         <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://github.com/lucasaraujo18.png" />
+            <Avatar hasBorder={false} src="https://github.com/jvdsantos3.png" />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
